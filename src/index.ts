f639ae45ebe8cd5a7cdb2f57483bd9895f4dc6fc
MattIPv4 <me@mattcowley.co.uk>
2023-06-14 21:17:19 +0100

    Return object directly, consistent arrow functions

--- src/index.ts
+++ src/index.ts
@@ -28,73 +28,70 @@ const esbuildPluginStyledComponents = ({
     transpileTemplateLiterals = false,
     pure = false,
     topLevelImportPaths = [],
-}: Options): Plugin => {
+}: Options): Plugin => ({
+    name: "styled-components",
+    setup: ({ onLoad, initialOptions }) => {
+        const root = process.cwd();
+        onLoad({ filter: new RegExp(filter) }, async (args) => {
+            // Read in the file
+            const code = await readFile(args.path, "utf8");
 
-    return {
-        name: "styled-components",
-        setup({ onLoad, initialOptions }) {
-            const root = process.cwd();
-            onLoad({ filter: new RegExp(filter) }, async (args) => {
-                // Read in the file
-                const code = await readFile(args.path, "utf8");
+            // Determine plugins to use
+            const plugins = [
+                "importMeta",
+                "topLevelAwait",
+                "classProperties",
+                "classPrivateProperties",
+                "classPrivateMethods",
+            ] as ParserPlugin[];
+            const isJsx = /\.[tj]sx$/.test(args.path);
+            if (isJsx) plugins.push("jsx");
+            const isTs = /\.tsx?$/.test(args.path);
+            if (isTs) plugins.push("typescript");
 
-                // Determine plugins to use
-                const plugins = [
-                    "importMeta",
-                    "topLevelAwait",
-                    "classProperties",
-                    "classPrivateProperties",
-                    "classPrivateMethods",
-                ] as ParserPlugin[];
-                const isJsx = /\.[tj]sx$/.test(args.path);
-                if (isJsx) plugins.push("jsx");
-                const isTs = /\.tsx?$/.test(args.path);
-                if (isTs) plugins.push("typescript");
-
-                // Run the code through babel
-                const map = initialOptions.sourcemap !== false;
-                const result = await transformAsync(code, {
-                    babelrc: false,
-                    configFile: false,
-                    ast: false,
-                    root,
-                    filename: args.path,
-                    parserOpts: {
-                        sourceType: "module",
-                        allowAwaitOutsideFunction: true,
-                        plugins,
-                    },
-                    generatorOpts: {
-                        decoratorsBeforeExport: true,
-                    },
-                    plugins: [[styled, {
-                        ssr,
-                        displayName,
-                        fileName,
-                        meaninglessFileNames,
-                        minify,
-                        transpileTemplateLiterals,
-                        pure,
-                        topLevelImportPaths,
-                    }]],
-                    sourceMaps: map,
-                });
-
-                // If babel fails to return, throw an error
-                if (!result)
-                    throw new Error(`Babel transformation failed for ${args.path}`);
-
-                // Return the transformed code to esbuild
-                return {
-                    contents: result.code + (result.map && map
-                        ? `//# sourceMappingURL=data:application/json;base64,${Buffer.from(JSON.stringify(result.map)).toString("base64")}`
-                        : ""),
-                    loader: `${isTs ? "ts" : "js"}${isJsx ? "x" : ""}`,
-                };
+            // Run the code through babel
+            const map = initialOptions.sourcemap !== false;
+            const result = await transformAsync(code, {
+                babelrc: false,
+                configFile: false,
+                ast: false,
+                root,
+                filename: args.path,
+                parserOpts: {
+                    sourceType: "module",
+                    allowAwaitOutsideFunction: true,
+                    plugins,
+                },
+                generatorOpts: {
+                    decoratorsBeforeExport: true,
+                },
+                plugins: [[styled, {
+                    ssr,
+                    displayName,
+                    fileName,
+                    meaninglessFileNames,
+                    minify,
+                    transpileTemplateLiterals,
+                    pure,
+                    topLevelImportPaths,
+                }]],
+                sourceMaps: map,
             });
-        },
-    };
-}
+
+            // If babel fails to return, throw an error
+            if (!result)
+                throw new Error(`Babel transformation failed for ${args.path}`);
+
+            // Return the transformed code to esbuild
+            return {
+                contents: result.code + (result.map && map
+                    ? `//# sourceMappingURL=data:application/json;base64,${Buffer.from(JSON.stringify(result.map)).toString("base64")}`
+                    : ""),
+                loader: `${isTs ? "ts" : "js"}${isJsx ? "x" : ""}`,
+            };
+        });
+    },
+});
 
 export default esbuildPluginStyledComponents;
 
